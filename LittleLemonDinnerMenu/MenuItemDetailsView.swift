87ec705daf1f4b5495e0d22968c7a488802ba1c3
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-07-19 15:02:03 -0400

    Details view completed

--- LittleLemonDinnerMenu/MenuItemDetailsView.swift
+++ LittleLemonDinnerMenu/MenuItemDetailsView.swift
@@ -7,22 +7,53 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct MenuItemDetailsView: View {
     let item: MenuItem
 
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            Text(item.title)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom)
+
             Image(item.imageName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-            HStack {
-                Text(item.title)
-                    .font(.headline)
-                Text("$\(item.price, specifier: "%.2f")")
-                    .font(.subheadline)
-            }
+                .padding(.bottom)
+            
             Text(item.description)
-                .font(.headline)
+                .font(.body)
+                .fontWeight(.medium)
+                .padding(.bottom)
+            
+            Text("$\(item.price, specifier: "%.2f")")
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding(.bottom)
+
+            Text("Ordered: \(item.ordersCount)")
+                .font(.body)
+                .fontWeight(.medium)
+                .padding(.bottom)
+            
+            Text("Ingredients:")
+                .font(.body)
+                .fontWeight(.medium)
+                .padding(.bottom)
+
+            ForEach(item.ingredients, id: \.self) { ingredient in
+                Text(ingredient.rawValue)
+                    .font(.body)
+            }
+            
+            Image("LittleLemonLogo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.bottom)
+
             Spacer()
         }
         .padding()
